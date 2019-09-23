3d55f82839768dc1e41c2fb2eead3fcae33e25c0
Randy Fay <randy@randyfay.com>
2019-09-23 13:42:56 -0600

    Try uploading assssets and waiting

--- site-test.sh
+++ site-test.sh
@@ -18,15 +18,30 @@ set -o nounset
 set -x
 
 # We'll want to add args later, but for now static configuration
+#GITHUB_REPO=rfay/d8composer
+#SITENAME="d8composer-test-$(date +%Y%m%d%H%M%S)"
 GITHUB_REPO=rfay/d8composer
-SITENAME="d8composer-test-$(date +%Y%m%d%H%M%S)"
+SITE_BASENAME=d8composer
+SITENAME="${SITE_BASENAME}-$(date +%Y%m%d%H%M%S)"
 
 function cleanup {
-    echo "Here are the cleanup actions"
+    set +x
+    echo "This would do cleanup actions including 'ddev-live delete site ${SITENAME}' at this point"
+    #ddev-live delete site ${SITENAME}
 }
 trap cleanup EXIT
 
 # Consider downloading and installing latest ddev-live-client and using it.
 
 echo "Creating site ${SITENAME}"
-ddev-live create site drupal ${SITENAME} --github-repo=${GITHUB_REPO} --run-composer-install
+ddev-live create site drupal ${SITENAME} --github-repo=${GITHUB_REPO} --run-composer-install --docroot web
+#ddev-live create site drupal ${SITENAME} --github-repo=${GITHUB_REPO} --drupal-version 7 --branch 7.x
+
+./wait_site_healthy.sh ${SITENAME}
+echo -ne '\007' >&2
+
+ddev-live push db ${SITENAME} assets/${SITE_BASENAME}.sql.gz
+pushd assets/${SITE_BASENAME}
+ddev-live push files ${SITENAME} .
+popd
+
--- wait_site_healthy.sh
+++ wait_site_healthy.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+sitename=${1-nosite}
+
+echo "waiting for site ${sitename} to become ready at $(date)" >&2
+
+for i in {1000..0}; do
+  status=$(ddev-live describe site ${sitename} | grep -v "Using org: " | jq -r .status.conditions[1].status)
+
+  if [ ${status} = "True" ]; then
+    echo "Site ${sitename} seems to have become ready at $(date) \007" >&2
+    exit 0
+  fi
+  printf "." >&2
+  sleep 10
+done
+
+echo "site ${sitename} never became ready, giving up at $(date) \007" >&2
+exit 2
