bf0691cac1e2dce2f015cd55d8f9b63149c99174
Randy Fay <randy@randyfay.com>
2020-05-18 16:08:10 -0600

    Add more healthcheck information when waiting for healthy site

--- wait_site_healthy.sh
+++ wait_site_healthy.sh
@@ -8,11 +8,22 @@ sitename=${1-nosite}
 
 echo "waiting for site ${sitename}: $(date)" >&2
 
-for i in {1000..0}; do
-#  status=$(ddev-live describe site ${sitename} | grep -v "Using org: " | jq -r .status.conditions[1].status)
-  previewUrl=$(ddev-live describe site ${sitename}  -o json | jq -r .previewUrl )
+for i in {10..0}; do
+    if ! ddev-live describe site ${sitename} >/dev/null 2>/dev/null ; then
+        echo -n "#"
+        sleep 1
+    fi
+done
+echo
 
-  if [ "${previewUrl}" != "" ]; then
+for i in {1000..0}; do
+  descOut=$(ddev-live describe site ${sitename} -o json)
+  previewUrl=$(echo "${descOut}" | ddev-live describe site ${sitename}  -o json | jq -r .previewUrl )
+  siteHealthy=$(echo "${descOut}" | ddev-live describe site ${sitename}  -o json | jq -r .site.healthy )
+  filestoreHealthy=$(echo "${descOut}" | ddev-live describe site ${sitename}  -o json | jq -r .fileStore.healthy )
+  databaseHealthy=$(echo "${descOut}" | ddev-live describe site ${sitename}  -o json | jq -r .database.healthy )
+
+  if [ "${previewUrl}" != "" ] && [ ${siteHealthy} = "true" ] && [ ${filestoreHealthy} = "true" ] && [ ${databaseHealthy} = "true" ]; then
     printf "\nSite ${sitename} seems to have become ready at $(date) \007\n" >&2
     exit 0
   fi
